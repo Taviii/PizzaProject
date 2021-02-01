15826d9719e171badaddab93bd363f67f66d662e
Tavi <56189672+s21354-pj@users.noreply.github.com>
2021-02-01 16:19:55 +0100

    2p (#2)
    Added PizzaRpository, database

--- src/main/java/pl/pjwstk/pizzeria/pizzeria/controller/PizzaController.java
+++ src/main/java/pl/pjwstk/pizzeria/pizzeria/controller/PizzaController.java
@@ -1,13 +1,12 @@
 package pl.pjwstk.pizzeria.pizzeria.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.pjwstk.pizzeria.pizzeria.model.Pizza;
 import pl.pjwstk.pizzeria.pizzeria.service.PizzaService;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/pizza")
@@ -22,6 +21,21 @@ public class PizzaController {
     @GetMapping
     public ResponseEntity<List<Pizza>> findAll(){
         return ResponseEntity.ok(pizzaService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Pizza>> findById(@PathVariable long id) {
+        Optional<Pizza> byId = pizzaService.findById(id);
+        if (byId.isPresent()) {
+            return ResponseEntity.ok(byId);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Pizza> save(@RequestBody Pizza pizza){
+        return ResponseEntity.ok(pizzaService.save(pizza));
     }
 
 }
--- src/main/java/pl/pjwstk/pizzeria/pizzeria/service/PizzaService.java
+++ src/main/java/pl/pjwstk/pizzeria/pizzeria/service/PizzaService.java
@@ -3,16 +3,41 @@ package pl.pjwstk.pizzeria.pizzeria.service;
 
 import org.springframework.stereotype.Service;
 import pl.pjwstk.pizzeria.pizzeria.model.Pizza;
+import pl.pjwstk.pizzeria.pizzeria.repository.PizzaRepository;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PizzaService {
 
+    public PizzaRepository pizzaRepository;
+
+    private PizzaService(PizzaRepository pizzaRepository) {
+        this.pizzaRepository = pizzaRepository;
+    }
+
     public List<Pizza> findAll(){
-        return  List.of(
-                new Pizza("Marinara", "mała", "czosnkowy", "cienkie"),
-                new Pizza("Cztery Sery", "średnia", "czosnkowy", "średnie"),
-                new Pizza("Carbonara", "duża", "pomidorowy", "cienkie"));
+        return pizzaRepository.findAll();
+    }
+
+    public Optional<Pizza> findById(long id){
+        List<Pizza> pizzas = new ArrayList<>();
+        pizzas.add(new Pizza(1L,"Marinara", "mała", "czosnkowy", "cienkie"));
+        pizzas.add(new Pizza(2L,"Cztery Sery", "średnia", "czosnkowy", "średnie"));
+        pizzas.add(new Pizza( 3L,"Carbonara", "duża", "pomidorowy", "cienkie"));
+
+        return pizzas.stream().filter(pizza -> pizza.getId() == id).findFirst();
+//        for (Pizza pizza: pizzas) {
+//            if (pizza.getId()==id){
+//                return pizza;
+//            }
+//        }
+//        return  new Pizza();
+    }
+
+    public Pizza save(Pizza pizza){
+        return  pizzaRepository.save(pizza);
     }
 }
